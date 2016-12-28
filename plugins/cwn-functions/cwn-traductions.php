1fa894460f0b4b0540dd9c9128cf46d74a0506e8
Manuel Schmalstieg <git.code@ms-studio.net>
2016-12-28 16:56:59 +0100

    Correctif des traductions custom
    - On utilise une nouvelle méthode de chargement des traductions, l’ancienne ne marchant plus depuis WP 4.6.
    - On n’utilise plus woo_pdf.
    - Ajout de traductions pour Subscriptio

--- plugins/cwn-functions/cwn-traductions.php
+++ plugins/cwn-functions/cwn-traductions.php
@@ -3,69 +3,38 @@
 /* Custom translation strings
  */
 
-
-// remove_action( 'bp_core_loaded', 'bp_core_load_buddypress_textdomain' );
-// = completely disables the buddypress translation
-
-add_action( 'plugins_loaded', 'cowork_load_textdomain', 1 ); // must be earlier than 10!
-
-function cowork_load_textdomain() {
-			
-			// woocommerce
-			
-			load_plugin_textdomain( 
-				'woocommerce',
-				false, 
-				'cwn-functions/languages/' //
-			);
-			
-			// subscriptio
-			
-			load_plugin_textdomain( 
-				'subscriptio',
-				false, 
-				'cwn-functions/languages/'
-			);
-			
-			// woocommerce-membership
-			
-			load_plugin_textdomain( 
-				'woocommerce-membership',
-				false, 
-				'cwn-functions/languages/'
-			);
-			
-			// woocommerce-pdf-invoice
-			
-			load_plugin_textdomain( 
-				'woo_pdf',
-				false, 
-				'cwn-functions/languages/'
-			);
-			
-}
-
-
-// A stronger override for WooCommerce
-
-// Code to be placed in functions.php of your theme or a custom plugin file.
-add_filter( 'load_textdomain_mofile', 'load_custom_plugin_translation_file', 10, 2 );
-
 /*
  * Replace 'textdomain' with your plugin's textdomain. e.g. 'woocommerce'. 
  * File to be named, for example, yourtranslationfile-en_GB.mo
  * File to be placed, for example, wp-content/lanaguages/textdomain/yourtranslationfile-en_GB.mo
  */
 function load_custom_plugin_translation_file( $mofile, $domain ) {
-
+	
+	// folder location
+	$folder = WP_PLUGIN_DIR . '/cwn-functions/languages/';
+	
+	// filename ending
+	$file = '-' . get_locale() . '.mo';
+	
   if ( 'woocommerce' === $domain ) {
-    // $mofile = WP_PLUGIN_DIR . '/textdomain/yourtranslationfile-' . get_locale() . '.mo';
-    $mofile = WP_PLUGIN_DIR . '/cwn-functions/languages/woocommerce-' . get_locale() . '.mo';
+  
+    $mofile = $folder.$domain.$file;
+  
+  } else if ( 'subscriptio' === $domain ) {
+  
+  	$mofile = $folder.$domain.$file;
+  	
+  } else if ( 'woocommerce-membership' === $domain ) {
+  
+  	$mofile = $folder.$domain.$file;
+  	
   }
+  
   return $mofile;
+
 }
 
-
+add_filter( 'load_textdomain_mofile', 'load_custom_plugin_translation_file', 10, 2 );
 
 
 // filtering for Active => Actif
