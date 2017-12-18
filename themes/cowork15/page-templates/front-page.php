780288498ebcb830de692e4ca059b4ae68c79c0d
Manuel Schmalstieg <git.code@ms-studio.net>
2017-12-18 22:39:06 +0100

    Suppression de la vue 360° Google sur homepage

--- themes/cowork15/page-templates/front-page.php
+++ themes/cowork15/page-templates/front-page.php
@@ -13,21 +13,6 @@ get_header(); ?>
 
 	<?php endwhile; ?>
 	
-	<?php 
-	
-	// GOOGLE BUSINESS TOUR
-	
-	 ?>
-	 
-	 <div class="iframe-container">
-		 <div class="iframe-container-title">
-			 <h2 class="h2 title-style">Visite virtuelle du coworking</h2>
-			 <h3 class="h3">réalisation: <a href="http://www.proview360.ch/">proview360.ch</a></h3>
-		 </div>
-	 
-	 <iframe id="front-google-tour" class="front-google-tour" src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sch!4v1442477971744!6m8!1m7!1sqmWjn5gYycsAAAQqhCoYLw!2m2!1d46.99445902322461!2d6.927632854255307!3f223.32!4f0!5f0.7820865974627469" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-	 </div>
-
 	<?php rewind_posts(); ?>
 	
 	<?php
