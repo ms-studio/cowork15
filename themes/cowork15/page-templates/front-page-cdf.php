70073a596f6fb942825aa52b19595a0dda392883
Manuel Schmalstieg <git.code@ms-studio.net>
2018-01-26 22:16:31 +0100

    on réduit le template "CdF"

--- themes/cowork15/page-templates/front-page-cdf.php
+++ themes/cowork15/page-templates/front-page-cdf.php
@@ -19,74 +19,10 @@ get_header(); ?>
 	
 	// PRESTATIONS
 	
-	$custom_query = new WP_Query( array(
-				'posts_per_page' => 1,
-				'page_id' => 883,
-		) ); 
-		
-		if ($custom_query->have_posts()) : 
-		
-			?><div class="front-item"><?php
-		
-		while( $custom_query->have_posts() ) : $custom_query->the_post();
-		
-				// title
-				
-				?>
-				<h2 id="prestations" class="h2 title-style">Prestations</a></h2>
-				<?php
-				
-				// content
-				
-				the_content('Read the rest of this entry &raquo;');
-				
-				edit_post_link( __( 'Edit', 'edin' ), '<footer class="entry-footer modify-link"><span class="edit-link">', '</span></footer>' );
-		
-		endwhile; 
-			
-			?></div><?php
-		
-		endif;
-		wp_reset_postdata();
-		
-		
-		// TARIFS
-			
-			$custom_query = new WP_Query( array(
-						'posts_per_page' => 1,
-						'page_id' => 878,
-				) ); 
-				
-				if ($custom_query->have_posts()) : 
-				
-					?><div class="front-item"><?php
-				
-				while( $custom_query->have_posts() ) : $custom_query->the_post();
-				
-						// title
-						
-						?>
-						<h2 id="tarifs" class="h2 title-style">Tarifs</a></h2>
-						<?php
-						
-						// content
-						
-						the_content('Read the rest of this entry &raquo;');
-						
-						edit_post_link( __( 'Edit', 'edin' ), '<footer class="entry-footer modify-link"><span class="edit-link">', '</span></footer>' );
-				
-				endwhile; 
-					
-					?></div><?php
-				
-				endif;
-				wp_reset_postdata();
 	
 	?>
 
-<?php edin_featured_pages(); 
-
-	
+<?php 
 ?>
 <?php get_sidebar( 'front-page' ); 
 
