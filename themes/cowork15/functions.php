9590618e0179134274ef71287c56a05b4987e4f3
Manuel Schmalstieg <git.code@ms-studio.net>
2018-01-26 22:53:12 +0100

    correctifs CSS

--- themes/cowork15/functions.php
+++ themes/cowork15/functions.php
@@ -6,7 +6,7 @@
  *
  */
 
-// Change-Detector-XXXX - for Espresso.app
+// Change-Detector-XXXXXXXXXX - for Espresso.app
 
 /**
  * Set the content width based on the theme's design and stylesheet.
@@ -247,7 +247,7 @@ if ( $host != 'coworking-neuchatel.ch' ) {
  		// PROD: the MAIN stylesheet - combined and minified
  		wp_enqueue_style( 
  				'cowork-style', 
- 				get_stylesheet_directory_uri() . '/css/build/styles.20180126221120.css', // main.css
+ 				get_stylesheet_directory_uri() . '/css/build/styles.20180126225144.css', // main.css
  				false, // dependencies
  				null // version
  		); 
