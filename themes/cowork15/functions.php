16ff7c4070f461c77c97851fc142c140e03dbd76
Manuel Schmalstieg <git.code@ms-studio.net>
2018-03-06 06:38:47 +0100

    ajout du code Facebook Pixel

--- themes/cowork15/functions.php
+++ themes/cowork15/functions.php
@@ -323,5 +323,34 @@ if ( is_user_logged_in() ) {
 		require_once('functions/admin.php');
 }
 
+/* Footer JS Code */
+
+
+function cwn_fb_pxl_script_footer(){ 
+
+?>
+
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1021491014558863');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1021491014558863&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+<?php } 
+
+add_action('wp_footer', 'cwn_fb_pxl_script_footer');
+
 
 
